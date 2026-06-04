2a375380aa034ee1c222ded682b4a55ace01252a
LjcYounger <LjcYounger@163.com>
2026-06-04 22:37:24 +0800

    ignore ./site

---
+++ unity_animation_player_time_test/__init__.py
@@ -0,0 +1,20 @@
+from .animation_player import AnimationPlayer
+from .pyside_animation_player import PysideAnimationPlayer
+from .animation_events import AnimationEvents
+from .kwargs import PlayKwargs, PlayKwargsDict, type_kwargs
+from .numba_interpolation import RationalBezierInterpolation
+from .config import USE_JIT
+
+if USE_JIT:
+    # Compile in advance
+    _ = RationalBezierInterpolation(0, 1, 0, 1, 0, 0, 1, 1, 1, 1)
+    _(0.5)
+
+__all__ = [
+    "AnimationPlayer",
+    "PysideAnimationPlayer",
+    "AnimationEvents",
+    "PlayKwargs",
+    "PlayKwargsDict",
+    "type_kwargs"
+]
